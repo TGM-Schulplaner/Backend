e8e6dcef23cdf22a2b8a8de9b29bc69374a861eb
Minecraftschurli <minecraftschurli@gmail.com>
2020-02-14 10:49:41 +0100

    redo structuring add spring data

---
+++ src/test/java/schulplaner/Tests.java
@@ -0,0 +1,13 @@
+package schulplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
