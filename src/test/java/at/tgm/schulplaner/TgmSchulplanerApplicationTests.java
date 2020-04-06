1541e468fbffa7e230ad469b76371a678a0710fc
Georg Burkl <gburkl@student.tgm.ac.at>
2020-04-06 22:54:55 +0200

    add test class
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

---
+++ src/test/java/at/tgm/schulplaner/TgmSchulplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tgm.schulplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgmSchulplanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
